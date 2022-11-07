31bdeeea3794023350f98cd3241b16f27e4406fc
Israel David <israelcaetano21@hotmail.com>
2022-11-07 20:12:13 -0300

    Authors: display images better

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.static("public")); // Serve static files (e.g. CSS files)
+app.use('/images', express.static("images"));
 
 app.use(blogRoutes);
 
