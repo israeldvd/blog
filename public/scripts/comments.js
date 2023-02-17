2535f598131fefa63b0893e3c76436577fbe29a4
Israel David <israelcaetano21@hotmail.com>
2023-02-17 19:50:49 -0300

    Posts: allow fetching a post's comments

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,11 +1,35 @@
 const loadCommentsBtnElem = document.getElementById("load-comments-btn");
-// const commentsListElem = document.getElementById("comments-list");
+const commentsUListElem = document.getElementById("comments-list");
+
+function createCommentsList(comments) {
+    const commentsListElement = document.createElement("ul");
+
+    for (const comment of comments) {
+        const commentElement = document.createElement("li");
+        commentElement.innerHTML = `
+            <article class="comment-item">
+                <h3>
+                    ${comment.title}
+                </h3>
+                <p>
+                    ${comment.message}
+                </p>
+            </article>`;
+        commentsListElement.appendChild(commentElement);
+    }
+
+    return commentsListElement;
+}
 
 async function fetchComments() {
     const postId = loadCommentsBtnElem.dataset.postid;
     const response = await fetch(`/posts/${postId}/comments`);
     const responseData = await response.json();
-    console.log(responseData);
+
+    commentsUListElem.innerHTML = '';
+    commentsUListElem.appendChild(
+        createCommentsList(responseData.comments)
+    );
 }
 
 loadCommentsBtnElem?.addEventListener("click", fetchComments);
